f74e3c70dac257c05fdff0dac0361b446f5b27d1
Evgeny <sobolenok119@mail.ru>
2021-11-09 15:42:13 +0300

    Added new blocks, their styles and scripts. Added webpage-validation and media queries. Added animations.

--- MYGARDEN-SITE (Completed)/js/script.js
+++ MYGARDEN-SITE (Completed)/js/script.js
@@ -80,3 +80,79 @@ const toggleItem = (item) => {
         item.classList.add('accordion-open');
     }
 };
+
+// Выделение активных блоков при скролле в меню
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        }
+        else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+// Видимость быстрого скролла вверх
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+
+    if (this.scrollY >= 200) {
+        scrollUp.classList.add('show-scroll');
+    }
+    else {
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+
+window.addEventListener('scroll', scrollUp);
+
+// Смена темы (Dark/Light)
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+// Подключение и настройка анимации, переходов между блоками и внутри них
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: false
+});
+
+sr.reveal(`.home__data`);
+sr.reveal(`.home__img`, {delay: 500});
+sr.reveal(`.home__social`, {delay: 600});
+sr.reveal(`.about__img, .contact__box`, {origin: 'left'});
+sr.reveal(`.about__data, .contact__form`, {origin: 'right'});
+sr.reveal(`.steps__card, .product__card, .questions__group, .footer`, {interval: 100});
